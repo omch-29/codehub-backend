15b396c6bdc500c6e38e04f09f5209ef9895cf21
omch-29 <chandwaniom29@gmail.com>
2026-04-06 18:53:05 +0530

    repo router

--- routes/repo.router.js
+++ routes/repo.router.js
@@ -115,8 +115,6 @@ repoRouter.get("/folder/:repoId", async (req, res) => {
 
 
 
-//     let chunks = [];
-//     for await (const chunk of data.Body) chunks.push(chunk);
 //     const content = Buffer.concat(chunks).toString("utf8");
 
 //     return res.json({ content });
